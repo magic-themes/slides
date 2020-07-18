748b0936277a5e47195407d0ca59619e9ec579f8
jascha <jascha@jaeh.at>
2020-07-18 03:49:48 +0200

    NextPrev: proper positioning

--- src/NextPrev.mjs
+++ src/NextPrev.mjs
@@ -1,10 +1,16 @@
 export const View = ({ next = false, prev = false, state }) =>
   div({ class: 'NextPrev' }, [
-    next && Link({ to: next === '/' ? state.root : `${state.root}${next}/`, text: 'next' }),
     prev && Link({ to: prev === '/' ? state.root : `${state.root}${prev}/`, text: 'prev' }),
+    next && Link({ to: next === '/' ? state.root : `${state.root}${next}/`, text: 'next' }),
   ])
 
 export const style = {
+  position: 'fixed',
+  bottom: '0.5em',
+  right: '0.5em',
+  left: 'auto',
+  top: 'auto',
+
   a: {
     margin: '0 1em',
   },
